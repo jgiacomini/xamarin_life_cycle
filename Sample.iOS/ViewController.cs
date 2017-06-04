acbf148d65713e512fb010c957fc0cbb0a904d94
jgiacomini <ismac@ismacs-Mac.local>
2017-06-04 12:25:47 +0200

    Ajout des méthodes du cycle de vie (iOS)

--- Sample.iOS/ViewController.cs
+++ Sample.iOS/ViewController.cs
@@ -10,10 +10,36 @@ namespace Sample.iOS
         {
         }
 
+
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+        }
+
+        public override void ViewWillDisappear(bool animated)
+        {
+            base.ViewWillDisappear(animated);
+        }
+
+        public override void ViewDidDisappear(bool animated)
+        {
+            base.ViewDidDisappear(animated);
+        }
+
+        public override void ViewDidUnload()
+        {
+            base.ViewDidUnload();
         }
 
         public override void DidReceiveMemoryWarning()
